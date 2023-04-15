2be76a4cf985d99ce64cdad2399cec1415cafd48
Pjijin <pjijin1@gmail.com>
2023-04-16 02:55:36 +0900

    🐛 Fix: updated prisma query

--- src/pages/api/account/live_stream.ts
+++ src/pages/api/account/live_stream.ts
@@ -5,22 +5,15 @@ export default async function Handler(req: NextApiRequest, res: NextApiResponse)
 
     const { wallet_address, stream_id } = req.body;
 
-    const user = await db.creators.findFirst({
+
+    await db.creators.updateMany({
         where: {
             wallet_address: wallet_address
+        },
+        data: {
+            stream_id: stream_id
         }
     })
-
-    if (user) {
-        await db.creators.update({
-            where: {
-                creator_id: user.creator_id
-            },
-            data: {
-                stream_id: stream_id
-            }
-        })
-    }
 
     return res.status(200).json({ success: true })
 
